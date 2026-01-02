bc04f08e65fb6d4b630579076ca371ede925114a
Victor Payno <vpayno@users.noreply.github.com>
2026-01-01 16:41:17 -0800

    nix(flake-browser): add flake browser fixes

--- resources/scripts/flake-browser.bash
+++ resources/scripts/flake-browser.bash
@@ -1,20 +1,20 @@
 # shellcheck shell=bash
 
-declare arg="''${1:-}"
+declare flake_uri="${1:-}"
 
-if [[ -z $arg ]]; then
+if [[ -z ${flake_uri} ]]; then
 	printf "ERROR: You must pass an absolute path or git url to nix-inspect.\n"
 	printf "\n"
 	exit 1
 fi 1>&2
 
-if [[ "$arg" == "." ]]; then
-	arg="$PWD"
+if [[ ${flake_uri} == . ]]; then
+	flake_uri="${PWD}"
 fi
 
 {
-	echo Running: nix-inspect --expr "\"builtins.getFlake \\\"$arg\\\"\""
+	echo Running: nix-inspect --expr "\"builtins.getFlake \\\"${flake_uri}\\\"\""
 	printf "\n"
 } 1>&2
 
-exec nix-inspect --expr "builtins.getFlake \"$arg\""
+exec nix-inspect --expr "builtins.getFlake \"${flake_uri}\""
