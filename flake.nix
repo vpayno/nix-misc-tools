09a1853a457af72869bbbafe7561a646620300f1
Victor Payno <vpayno@users.noreply.github.com>
2026-01-11 14:42:46 -0800

    release(20260101.0.4): fix apps generation function

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
       system:
       let
         pname = "nix-misc-tools";
-        version = "20260101.0.3";
+        version = "20260101.0.4";
         name = "${pname}-${version}";
 
         flake_repo_url = "github:vpayno/nix-misc-tools";
