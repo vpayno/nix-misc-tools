48924d9297cfc0508430b85d3f01faa78a80dc95
Victor Payno <vpayno.oss@gmail.com>
2026-08-24 20:17:24 -0700

    chore(release): bump version to 20260824.0.0

--- flake.nix
+++ flake.nix
@@ -40,7 +40,7 @@
       system:
       let
         pname = "nix-misc-tools";
-        version = "20260822.0.0";
+        version = "20260824.0.0";
         name = "${pname}-${version}";
 
         flake_repo_url = "github:vpayno/nix-misc-tools";
